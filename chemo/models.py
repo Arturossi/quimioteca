7b95da083ee689c7be5d7c821746ad795469a960
DIEGO ENRY GOMES <dgomes@live.com>
2019-07-29 13:52:03 -0300

    Improved code readability

--- chemo/models.py
+++ chemo/models.py
@@ -24,13 +24,13 @@ class Users(models.Model):
         ("Not specified", "Not specified"),
     )
 
-    userID = models.AutoField(primary_key=True)
-    fullName = models.CharField(max_length=400)
-    email = models.CharField(max_length=256, unique=True) # Biggest email should be 254 (I've put +2 just to be power of 2)
-    gender = models.CharField(max_length=15, choices=GENDERS)
-    birthDate = models.DateField()
+    userID        = models.AutoField(primary_key=True)
+    fullName      = models.CharField(max_length=400)
+    email         = models.CharField(max_length=256, unique=True) # Biggest email should be 254 (I've put +2 just to be power of 2)
+    gender        = models.CharField(max_length=15, choices=GENDERS)
+    birthDate     = models.DateField()
     userCreatedAt = models.DateField()
-    countryID = models.ForeignKey(Countries, on_delete=models.CASCADE)
+    countryID     = models.ForeignKey(Countries, on_delete=models.CASCADE)
 
     # Metadata
     class Meta:
@@ -42,9 +42,9 @@ class Users(models.Model):
         return ', '.join('{}{}'.format(key, val) for key, val in self.__dict__.items())
 
 class Orders(models.Model):
-    orderID = models.AutoField(primary_key=True)
-    userID = models.ForeignKey(Users, on_delete=models.CASCADE)
-    status = models.CharField(max_length=100)
+    orderID        = models.AutoField(primary_key=True)
+    userID         = models.ForeignKey(Users, on_delete=models.CASCADE)
+    status         = models.CharField(max_length=100)
     orderCreatedAt = models.DateField()
 
     # Metadata
@@ -58,8 +58,8 @@ class Orders(models.Model):
 
 class OrderItems(models.Model):
     orderItemID = models.AutoField(primary_key=True)
-    productID = models.ForeignKey(Users, on_delete=models.CASCADE)
-    quantitity = models.PositiveIntegerField()
+    productID   = models.ForeignKey(Users, on_delete=models.CASCADE)
+    quantitity  = models.PositiveIntegerField()
 
     # Metadata
     class Meta:
@@ -71,11 +71,11 @@ class OrderItems(models.Model):
         return ', '.join('{}{}'.format(key, val) for key, val in self.__dict__.items())
 
 class Merchants(models.Model):
-    merchantID = models.AutoField(primary_key=True)
-    merchantName = models.CharField(max_length=1000)
-    countryID = models.ForeignKey(Countries, on_delete=models.CASCADE)
+    merchantID        = models.AutoField(primary_key=True)
+    merchantName      = models.CharField(max_length=1000)
+    countryID         = models.ForeignKey(Countries, on_delete=models.CASCADE)
     merchantCreatedAt = models.DateField()
-    userID = models.ForeignKey(Users, on_delete=models.CASCADE)
+    userID            = models.ForeignKey(Users, on_delete=models.CASCADE)
 
     # Metadata
     class Meta:
@@ -87,11 +87,11 @@ class Merchants(models.Model):
         return ', '.join('{}{}'.format(key, val) for key, val in self.__dict__.items())
 
 class Products(models.Model):
-    productID = models.AutoField(primary_key=True)
-    merchantID = models.ForeignKey(Merchants, on_delete=models.CASCADE)
-    name = models.CharField(max_length=250)
-    price = models.FloatField()
-    status = models.CharField(max_length=100)
+    productID        = models.AutoField(primary_key=True)
+    merchantID       = models.ForeignKey(Merchants, on_delete=models.CASCADE)
+    name             = models.CharField(max_length=250)
+    price            = models.FloatField()
+    status           = models.CharField(max_length=100)
     productCreatedAt = models.DateField()
 
     # Metadata
@@ -104,25 +104,25 @@ class Products(models.Model):
         return ', '.join('{}{}'.format(key, val) for key, val in self.__dict__.items())
 
 class Compounds(models.Model):
-    moleculeName = models.CharField(max_length=100) # 2i4x
-    totalMolweight = models.FloatField() # 728.794
-    cLogP = models.FloatField() # 2.8511
-    cLogS = models.FloatField() # -5.343
-    hAcceptors = models.PositiveIntegerField() # 14
-    hDonors = models.PositiveIntegerField() # 2
-    totalSurfaceArea = models.FloatField() # 534.30
-    polarSurfaceArea = models.FloatField() # 186.58
-    mutagenic = models.CharField(max_length=10) # none
-    tumorigenic = models.CharField(max_length=10) # none
-    irritant = models.CharField(max_length=10) # none
-    nonHAtoms = models.PositiveIntegerField() # 49
-    stereoCenters = models.PositiveIntegerField() # 5
-    rotatableBonds = models.PositiveIntegerField() # 19
-    smiles = models.CharField(max_length=5000) # CCOP(COc1ccc(C[C@@H]([C@@H](CN(CC(C)C)S(c(cc2)ccc2OC)(=O)=O)O)NC(O[C@@H]2[C@H](CCO3)[C@H]3OC2)=O)cc1)(OCC)=O
-    inChI = models.CharField(max_length=5000) # InChI=1S/C33H49N2O12PS/c1-6-45-48(38,46-7-2)22-44-26-10-8-24(9-11-26)18-29(34-33(37)47-31-21-43-32-28(31)16-17-42-32)30(36)20-35(19-23(3)4)49(39,40)27-14-12-25(41-5)13-15-27/h8-15,23,28-32,36H,6-7,16-22H2,1-5H3,(H,34,37)/t28-,29-,30+,31-,32-/m0/s1
-    inChIKey = models.CharField(max_length=1000) # FCLYPCIMVVLLRN-LUKCZKMGSA-N
-    provider = models.CharField(max_length=100, default='unkown') # nome do lab
-    numAtoms = models.IntegerField() # 50
+    moleculeName      = models.CharField(max_length=100) # 2i4x
+    totalMolweight    = models.FloatField() # 728.794
+    cLogP             = models.FloatField() # 2.8511
+    cLogS             = models.FloatField() # -5.343
+    hAcceptors        = models.PositiveIntegerField() # 14
+    hDonors           = models.PositiveIntegerField() # 2
+    totalSurfaceArea  = models.FloatField() # 534.30
+    polarSurfaceArea  = models.FloatField() # 186.58
+    mutagenic         = models.CharField(max_length=10) # none
+    tumorigenic       = models.CharField(max_length=10) # none
+    irritant          = models.CharField(max_length=10) # none
+    nonHAtoms         = models.PositiveIntegerField() # 49
+    stereoCenters     = models.PositiveIntegerField() # 5
+    rotatableBonds    = models.PositiveIntegerField() # 19
+    smiles            = models.CharField(max_length=5000) # CCOP(COc1ccc(C[C@@H]([C@@H](CN(CC(C)C)S(c(cc2)ccc2OC)(=O)=O)O)NC(O[C@@H]2[C@H](CCO3)[C@H]3OC2)=O)cc1)(OCC)=O
+    inChI             = models.CharField(max_length=5000) # InChI=1S/C33H49N2O12PS/c1-6-45-48(38,46-7-2)22-44-26-10-8-24(9-11-26)18-29(34-33(37)47-31-21-43-32-28(31)16-17-42-32)30(36)20-35(19-23(3)4)49(39,40)27-14-12-25(41-5)13-15-27/h8-15,23,28-32,36H,6-7,16-22H2,1-5H3,(H,34,37)/t28-,29-,30+,31-,32-/m0/s1
+    inChIKey          = models.CharField(max_length=1000) # FCLYPCIMVVLLRN-LUKCZKMGSA-N
+    provider          = models.CharField(max_length=100, default='unkown') # nome do lab
+    numAtoms          = models.IntegerField() # 50
 
 class Molecules(models.Model):
     moleculeID = models.AutoField(primary_key=True)
@@ -138,18 +138,18 @@ class Molecules(models.Model):
         return ', '.join('{}{}'.format(key, val) for key, val in self.__dict__.items())
 
 class Properties(models.Model):
-    propertyID = models.AutoField(primary_key=True)
-    moleculeID = models.ForeignKey(Molecules, on_delete=models.CASCADE)
-    pka = models.FloatField()
-    charge = models.FloatField()
-    molarMass = models.FloatField()
-    monoIsotropicMass = models.FloatField()
-    clogp = models.FloatField()
-    tpsa = models.FloatField()
-    lipinski = models.SmallIntegerField()
-    hBond = models.SmallIntegerField()
-    hBondDonnors = models.SmallIntegerField()
-    rotatableBonds = models.SmallIntegerField()
+    propertyID          = models.AutoField(primary_key=True)
+    moleculeID          = models.ForeignKey(Molecules, on_delete=models.CASCADE)
+    pka                 = models.FloatField()
+    charge              = models.FloatField()
+    molarMass           = models.FloatField()
+    monoIsotropicMass   = models.FloatField()
+    clogp               = models.FloatField()
+    tpsa                = models.FloatField()
+    lipinski            = models.SmallIntegerField()
+    hBond               = models.SmallIntegerField()
+    hBondDonnors        = models.SmallIntegerField()
+    rotatableBonds      = models.SmallIntegerField()
 
     # Metadata
     class Meta:
@@ -162,10 +162,10 @@ class Properties(models.Model):
 
 class Structures(models.Model):
     structureID = models.AutoField(primary_key=True)
-    moleculeID = models.ForeignKey(Molecules, on_delete=models.CASCADE)
-    smiles = models.CharField(max_length=5000)
-    inChi = models.CharField(max_length=5000)
-    inChiKey = models.CharField(max_length=14)
+    moleculeID  = models.ForeignKey(Molecules, on_delete=models.CASCADE)
+    smiles      = models.CharField(max_length=5000)
+    inChi       = models.CharField(max_length=5000)
+    inChiKey    = models.CharField(max_length=14)
 
     # Metadata
     class Meta:
@@ -177,11 +177,11 @@ class Structures(models.Model):
         return ', '.join('{}{}'.format(key, val) for key, val in self.__dict__.items())
 
 class Figures(models.Model):
-    figureID = models.AutoField(primary_key=True)
-    moleculeID = models.ForeignKey(Molecules, on_delete=models.CASCADE)
-    molecule = models.CharField(max_length=5000)
-    surface = models.CharField(max_length=5000)
-    pka = models.FloatField()
+    figureID    = models.AutoField(primary_key=True)
+    moleculeID  = models.ForeignKey(Molecules, on_delete=models.CASCADE)
+    molecule    = models.CharField(max_length=5000)
+    surface     = models.CharField(max_length=5000)
+    pka         = models.FloatField()
 
     # Metadata
     class Meta:
